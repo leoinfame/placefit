c6115bec3a99cd166de39a8c0488dca0389a889a
Leandro Santos Costa <fatuex@gmail.com>
2026-03-16 00:01:33 +0000

    File changes

--- src/pages/AtendenteIARevendedor.jsx
+++ src/pages/AtendenteIARevendedor.jsx
@@ -791,6 +791,7 @@ precisas sobre políticas, procedimentos, FAQ, etc.
           <TabsContent value="whatsapp" className="space-y-4">
             <WhatsAppSetup userId={user?.id} userType="revendedor" />
           </TabsContent>
+        </Tabs>
 
         {/* Dialog de Conhecimento */}
         <Dialog open={showDialog} onOpenChange={(open) => {
